fe6123253fb04255ac98bd90fc37aadf160f06e0
Cayanhs <huygneszindocsgo@gmail.com>
2025-05-07 22:02:25 -0300

    paia

--- atividadegithub.py
+++ atividadegithub.py
@@ -8,3 +8,16 @@ lista =[
 
 
 ]
+
+def cadastro(nome, idade, lista):
+    lista.append({'nome' : nome, 'idade' : idade})
+
+while True:
+    esc = input('1-Cadastrar cliente\n2-Buscar cliente\n3-Deletar\n4-Sair')
+
+    if esc == 'Cadastrar' or 'cadastrar':
+        cadastro(input('Nome: '), input('Idade: '), lista)
+
+
+    if esc == 'Sair' or 'sair':
+        break
