16f0bfda29876290364c005e71c4012f1bee7c43
AlexyaCC50 <alexyabbs@gmail.com>
2025-05-14 18:33:36 -0300

    Update 0.2

--- atividadegithub.py
+++ atividadegithub.py
@@ -8,16 +8,39 @@ lista =[
 
 
 ]
+true_while = True
+
+def buscar_cliente(nome,lista):
+    for dicionario in lista:
+
+        if dicionario['nome'] == nome:
+            print('Existe.')
+        else:
+            print('Não existe.')
 
 def cadastro(nome, idade, lista):
     lista.append({'nome' : nome, 'idade' : idade})
 
-while True:
-    esc = input('1-Cadastrar cliente\n2-Buscar cliente\n3-Deletar\n4-Sair')
+while true_while:
+    esc = input('1-Cadastrar cliente\n2-Buscar cliente\n3-Deletar\n4-Sair: ')
 
-    if esc == 'Cadastrar' or 'cadastrar':
-        cadastro(input('Nome: '), input('Idade: '), lista)
+    if esc == '1':
+        nome = input('Nome: ')
+        idade = input('Idade: ')
+        cadastro(nome, idade, lista)
+        print(lista)
+        print('')
 
 
-    if esc == 'Sair' or 'sair':
-        break
+
+
+    if esc == '2':
+        quem = input('Quem buscar?: ')
+        buscar_cliente(nome=quem, lista=lista)
+        print('')
+
+
+
+    if esc == '4':
+        true_while = False
+        
